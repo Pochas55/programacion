2f077c977956af7b5d5632eca2491de1a1a37447
Jonathan MirCha <jonmircha@gmail.com>
2016-09-10 11:57:13 -0500

    Clase 5 JS

--- JS/js/07_ajax_json.js
+++ JS/js/07_ajax_json.js
@@ -14,10 +14,35 @@
 				var resAJAX = '<p>Información extraída del archivo JSON sin analizar <mark>' + ajax.responseText + '</mark></p>',
 					resString = '<p>Información extraída del archivo JSON analizada como string <mark>' + j.stringify(ajax.responseText) + '</mark></p>',
 					resObject = '<p>Información extraída del archivo JSON analizada como objeto JS <mark>' + j.parse(ajax.responseText) + '</mark></p>';
-				moviesTemplate = resAJAX + resString + resObject,
+				
 				moviesInfo = j.parse(ajax.responseText);
-
+				moviesTemplate = resAJAX + resString + resObject;
 				console.log(ajax, moviesInfo);
+
+				//template string - ES6 o ES2015
+				//interpolación de variable `Hola ${nombreVariable}`
+				/*
+				moviesTemplate += `
+					<article>
+						<h2>${moviesInfo.title}</h2>
+						<p><b>${moviesInfo.year}</b></p>
+						<p><i>${moviesInfo.genres}</i></p>
+						<img src="${moviesInfo.poster}">
+					</article>
+				`;
+				*/
+
+				for (var i = 0; i < moviesInfo['movies'].length; i++) {
+					moviesTemplate += `
+						<article>
+							<h2>${moviesInfo['movies'][i].title}</h2>
+							<p><b>${moviesInfo['movies'][i].year}</b></p>
+							<p><i>${moviesInfo['movies'][i].genres}</i></p>
+							<img src="${moviesInfo['movies'][i].poster}">
+						</article>
+					`;
+				};
+
 			} else {
 				moviesTemplate = '<h3>El servidor NO responde. Error N°' + ajax.status + ': <mark>' + ajax.statusText + '</mark></h3>';
 			}
