6eec38bdda8e442f3faf65c0a2094d6723b08332
Jonathan MirCha <jonmircha@gmail.com>
2016-10-29 13:30:34 -0500

    Clase 1 PHP

--- PHP/POO/Perro.php
+++ PHP/POO/Perro.php
@@ -9,8 +9,68 @@ class Perro {
 
 			static: Pueden ser accedidos sin necesidad de instanciar un objeto y su valor es estático (no cambia)
 	*/
-	
+
 	//ATRIBUTOS
+	public $nombre;
+	public $edad;
+	public $raza;
+	public $genero;
+	public $esterilizado;
+	public static $mejor_cualidad = 'Fidelidad';
+	public static $mejor_amigo = 'Hombre';
+
+	//MÉTODOS MÁGICOS
+	//http://php.net/manual/es/language.oop5.magic.php
+
+	//CONSTRUCTOR: método que se ejecuta automáticamente al inicio de instanciar la clase
+	public function __construct() {
+
+	}
+
+	//DESTRUCTOR: método que se ejecuta automáticamente al final de instanciar la clase
+	public function __destruct() {
+
+	}
 
 	//MÉTODOS
+	public function ladrar() {
+
+	}
+
+	public function comer($comida) {
+
+	}
+
+	public function aparecer($imagen) {
+
+	}
+
+	public static function que_es_un_perro() {
+		/*
+			:: es el operador de resolución de ámbito, se puede usar con el nombre de la clase o con la palabra reservada self.
+			Sirve para invocar:
+				Fuera de la Clase
+					Métodos Estáticos
+					Atributos Estáticos
+					Constantes
+				Dentro de la Clase
+					Métodos Estáticos o No Estáticos
+					Atributos Estáticos
+					Constantes
+		*/
+		echo '
+			<p>
+				El perro o perro doméstico (Canis lupus familiaris)1 2 3 o también llamado can4 es un mamífero carnívoro de la familia de los cánidos, que constituye una subespecie del lobo (Canis lupus).
+			</p>
+			<p>
+				Un estudio publicado por la revista Nature revela que, gracias al proceso de domesticación, el organismo del perro se ha adaptado a cierta clase de alimentos, en este caso el almidón.
+			</p>
+			<p>
+				Su tamaño o talla, su forma y pelaje es muy diverso según la raza. Posee un oído y olfato muy desarrollados, siendo este último su principal órgano sensorial.
+			</p>
+			<p>
+				En las razas pequeñas puede alcanzar una longevidad de cerca de 20 años, con atención esmerada por parte del propietario, de otra forma su vida en promedio es alrededor de los 15 años.
+			</p>
+		';
+	}
 }
