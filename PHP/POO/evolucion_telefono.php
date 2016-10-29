da39a98c24f900878539cc83fe679a536a71d517
Jonathan MirCha <jonmircha@gmail.com>
2016-10-29 14:56:52 -0500

    Clase 1 PHP

--- PHP/POO/evolucion_telefono.php
+++ PHP/POO/evolucion_telefono.php
@@ -1,4 +1,38 @@
-<?php 
+<?php
+class Telefono {
+	public $marca;
+	public $modelo;
+	protected $medio = 'alámbrico';
+	protected $transmision = 'analógica';
+
+	public function __construct($marca, $modelo) {
+		$this->marca = $marca;
+		$this->modelo = $modelo;
+	}
+
+	public function llamar() {
+		echo '<p>Riiing Riiiing!!!</p>';
+	}
+
+	public function imprime_info() {
+		echo '
+			<ul>
+				<li>' . $this->marca . '</li>
+				<li>' . $this->modelo . '</li>
+				<li>' . $this->medio . '</li>
+				<li>' . $this->transmision . '</li>
+			</ul>
+		';
+	}
+}
+
+//HERENCIA: Los objetos pueden heredar propiedades y métodos de otros, mediante la extensión (herencia) de clases, cuya característica representa la relación existente entre diferentes objetos. Para definir una clase como extención de otra se utiliza la palabra clave extends
+
+class Celular extends Telefono {
+
+}
+
+
 echo '<h2>Tipos de Clases en PHP</h2>';
 /*
 	Tipos de clases en PHP:
@@ -12,3 +46,12 @@ echo '<h2>Tipos de Clases en PHP</h2>';
 			Sirven de modelo para otras, NO se pueden instanciar y DEBEN heredarse
 */
 echo '<img src="http://bextlan.com/img/para-cursos/tipos-clases-php.png">';
+
+
+echo '<h1>Evolución del Teléfono</h1>';
+
+
+echo '<h2>Teléfono:</h2>';
+$tel_casa = new Telefono('Panasonic', 'KX-TS550');
+$tel_casa->llamar();
+$tel_casa->imprime_info();
