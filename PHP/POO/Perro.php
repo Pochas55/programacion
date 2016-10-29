6a72586cc1c4ce689465d43069470d7e2fd149e2
Jonathan MirCha <jonmircha@gmail.com>
2016-10-29 14:14:38 -0500

    Clase 1 PHP

--- PHP/POO/Perro.php
+++ PHP/POO/Perro.php
@@ -16,6 +16,7 @@ class Perro {
 	public $raza;
 	public $genero;
 	public $esterilizado;
+	private $pulgas;
 	public static $mejor_cualidad = 'Fidelidad';
 	public static $mejor_amigo = 'Hombre';
 
@@ -23,26 +24,38 @@ class Perro {
 	//http://php.net/manual/es/language.oop5.magic.php
 
 	//CONSTRUCTOR: método que se ejecuta automáticamente al inicio de instanciar la clase
-	public function __construct() {
+	public function __construct($n, $e, $r, $g, $es, $p) {
+		echo '<p><mark>Hola soy el Constructor de la Clase</mark></p>';
 
+		$this->nombre = $n;
+		$this->edad = $e;
+		$this->raza = $r;
+		$this->genero = ($g) ? 'Macho' : 'Hembra';
+		$this->esterilizado = ($es) ? 'Esterilizado' : 'No Esterilizado';
+		$this->pulgas = ($p) ? 'Tiene pulgas' : 'Desinfectado';
 	}
 
 	//DESTRUCTOR: método que se ejecuta automáticamente al final de instanciar la clase
 	public function __destruct() {
-
+		echo '<p><mark>Adios soy el Destructor de la Clase</mark></p>';
+		unset($this);
 	}
 
 	//MÉTODOS
 	public function ladrar() {
-
+		echo '<p>guau guaau !!!</p>';
 	}
 
-	public function comer($comida) {
-
+	public function comer($comida = 'croquetas') {
+		echo '<p>' . $this->nombre . ' come ' . $comida . '</p>';
 	}
 
 	public function aparecer($imagen) {
+		echo '<img src="' . $imagen . '">';
+	}
 
+	public function tiene_pulgas() {
+		echo '<p>' . $this->pulgas . '</p>';
 	}
 
 	public static function que_es_un_perro() {
@@ -60,7 +73,7 @@ class Perro {
 		*/
 		echo '
 			<p>
-				El perro o perro doméstico (Canis lupus familiaris)1 2 3 o también llamado can4 es un mamífero carnívoro de la familia de los cánidos, que constituye una subespecie del lobo (Canis lupus).
+				El perro o perro doméstico (Canis lupus familiaris) o también llamado can es un mamífero carnívoro de la familia de los cánidos, que constituye una subespecie del lobo (Canis lupus).
 			</p>
 			<p>
 				Un estudio publicado por la revista Nature revela que, gracias al proceso de domesticación, el organismo del perro se ha adaptado a cierta clase de alimentos, en este caso el almidón.
@@ -72,5 +85,30 @@ class Perro {
 				En las razas pequeñas puede alcanzar una longevidad de cerca de 20 años, con atención esmerada por parte del propietario, de otra forma su vida en promedio es alrededor de los 15 años.
 			</p>
 		';
+
+		Perro::ladrar();
+		self::ladrar();
 	}
 }
+
+//Puedo acceder a estos métodos y atributos sin instanciar la clase porque son Estáticos
+Perro::que_es_un_perro();
+echo '<p>Mejor Cualidad de los perros: ' . Perro::$mejor_cualidad . '</p>';
+echo '<p>Mejor Amigo de los perros: ' . Perro::$mejor_amigo . '</p>';
+
+//Instanciar un objeto de mi clase
+$kenai = new Perro('kEnAi', 4, 'Callejero', true, true, false);
+//echo $kenai;
+var_dump($kenai);
+echo '<h1>' . $kenai->nombre . '</h1>';
+echo '<h2>' . $kenai->raza . '</h2>';
+echo '<h3>' . $kenai->edad . '</h3>';
+echo '<h4>' . $kenai->genero . '</h4>';
+echo '<h5>' . $kenai->esterilizado . '</h5>';
+//echo '<h6>' . $kenai->pulgas . '</h6>';
+
+$kenai->ladrar();
+$kenai->comer();
+$kenai->comer('tacos');
+$kenai->aparecer('http://jonmircha.github.io/slides-poo-js/img/kenai.jpg');
+$kenai->tiene_pulgas();
---
+++ PHP/POO/evolucion_telefono.php
@@ -0,0 +1,14 @@
+<?php 
+echo '<h2>Tipos de Clases en PHP</h2>';
+/*
+	Tipos de clases en PHP:
+		Instanciables
+			Ser instanciada (se pueden crear objetos) y/o se pueden heredar
+		Heredadas
+			Guardar relación con métodos y atributos de otra
+		Finales
+			Es Instanciables pero no se puede heredar
+		Abstractas
+			Sirven de modelo para otras, NO se pueden instanciar y DEBEN heredarse
+*/
+echo '<img src="http://bextlan.com/img/para-cursos/tipos-clases-php.png">';
