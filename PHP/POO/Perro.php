b8462c0b9b803c3b9423828116969614839840b7
Jonathan MirCha <jonmircha@gmail.com>
2016-10-29 13:12:02 -0500

    Clase 1 PHP

---
+++ PHP/POO/Perro.php
@@ -0,0 +1,16 @@
+<?php
+class Perro {
+	/*
+		Modificadores de Acceso
+		Los métodos y atributos de una clase pueden tener diferentes níveles de acceso, los cuales pueden ser:
+			public: Acceso desde cualquier método de la clase u objeto que lo invoque
+			private: Acceso sólo desde los métodos de la clase, los objetos no los pueden invocar
+			protected: Acceso sólo desde los métodos de la clase y subclases que la hereden, los objetos no los pueden invocar
+
+			static: Pueden ser accedidos sin necesidad de instanciar un objeto y su valor es estático (no cambia)
+	*/
+	
+	//ATRIBUTOS
+
+	//MÉTODOS
+}
