7a92d8b77e96a0d9904423e7af2aeaccda6bcd1d
Jonathan MirCha <jonmircha@gmail.com>
2017-02-11 16:01:43 -0600

    Clase 3 Móviles

---
+++ Moviles/Template/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>Mediasmart</title>
+    <link rel="stylesheet" href="./css/my_framework.css">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <article class="HomeBackground  container-fluid">
+        Video / Imagen de fondo más titulares
+    </article>
+    <article class="DoYouWant  container-fluid">
+        <section class="container  lg-flex  lg-flex-wrap">
+            <div class="item  xs-w100  lg-w50">Texto</div>
+            <div class="item  xs-w100  lg-w50  lg-order--1">Imagen</div>
+            <div class="item  xs-w100  lg-w50">Texto</div>
+            <div class="item  xs-w100  lg-w50  u-xs-none  u-lg-block">Imagen</div>
+        </section>
+    </article>
+    <article class="Quality  container-fluid">
+        <section class="container  lg-flex  lg-flex-wrap">
+            <div class="item  xs-w100">texto</div>
+            <div class="item  xs-w100  lg-w50  xl-c4">dato 1</div>
+            <div class="item  xs-w100  lg-w50  xl-c4">dato 2</div>
+            <div class="item  xs-w100  xl-c4">dato 3</div>
+        </section>
+    </article>
+    <article class="Testimony  container-fluid">
+        <section class="container  lg-flex  lg-flex-wrap">
+            <div class="item  xs-w100  lg-c4">Testimonio 1</div>
+            <div class="item  xs-w100  lg-c4">Testimonio 2</div>
+            <div class="item  xs-w100  lg-c4">Testimonio 3</div>
+        </section>
+    </article>
+    <article class="Contact  container-fluid">
+        <section class="container">
+            <div class="item  xs-w100">Botón Contacto</div>
+        </section>
+    </article>
+    <nav class="FooterNav  container-fluid">
+        <ul class="container  md-flex">
+            <li class="item  xs-w100  md-w25">Get In Touch</li>
+            <li class="item  xs-w100  md-w25">Producto</li>
+            <li class="item  xs-w100  md-w25">About us</li>
+            <li class="item  xs-w100  md-w25">Latest on the blog</li>
+        </ul>
+    </nav>
+    <footer class="Footer  container-fluid">
+        <section class="container  md-flex">
+            <div class="item  xs-w100  md-w20">&copy;</div>
+            <nav class="item  xs-w100  md-w60">Menú Píe</nav>
+            <nav class="item  xs-w100  md-w20">Redes Sociales</nav>
+        </section>
+    </footer>
+</body>
+</html>
